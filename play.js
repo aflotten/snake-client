b0ca2362f463f8e9ae154efdcaae40e1569fdf4e
Anders Flotten <anders3835@hotmail.com>
2022-10-19 21:25:59 +0000

    implemented handle user input function to exit game

--- play.js
+++ play.js
@@ -1,17 +1,8 @@
-const connect = require("./client");
-
-const handleUserInput = function() {
-  console.log(data)
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.on("data", handleUserInput);
-  stdin.resume();
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
+
 connect();
+
+setupInput();
